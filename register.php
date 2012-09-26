d39b1c36b2b68753590f8b1f6d7351352e41c551
Luis Vazquez <luis.g.vazquez@hotmail.com>
2012-09-25 22:18:40 -0500

    Add register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+//starting register
+
+$name = "luis";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8" />
+	</head>
+	
+	<body>
+		<?php 
+		echo "hello {$name}";
+		?>
+	
+	</body>
+
+</html>
